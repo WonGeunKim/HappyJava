32a4c9d222516cfafa1511ad0809e2dd004e5cc4
WonGeunKim <kwg4764@naver.com>
2023-05-24 20:52:54 +0900

    - 타입변환

--- src/NumberExam.java
+++ src/NumberExam.java
@@ -18,5 +18,20 @@ public class NumberExam {
         // 오버플로우
         System.out.println(maxInt + 1);
 
+
+        double d1 = 50;
+        double d2 = 500L;
+
+        System.out.println(d1);
+        System.out.println(d2);
+
+
+//        int i1 = 50.0;
+//        int i2 = 25.6f;
+        int i1 = (int)50.0;
+        int i2 = (int)25.6f;    // 잘림현상
+
+        System.out.println(i1);
+        System.out.println(i2);
     }
 }
---
+++ src/PrimitiveCastExam.java
@@ -0,0 +1,18 @@
+public class PrimitiveCastExam {
+
+    public static void main(String[] args) {
+
+        long x2 = 50;
+        int i2 = (int)x2;
+
+        System.out.println(x2);
+        System.out.println(i2);
+
+        long x3 = Long.MAX_VALUE;
+        int i3 = (int)x3;
+
+        System.out.println(x3);
+        System.out.println(i3);     // 음수 : -1
+
+    }
+}
